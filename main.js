765c8676f2fd0ee7b483e1610d9156a0fd23fb7e
Fabien Marceau <fabien.marceau@gmail.com>
2023-03-03 11:23:44 +0100

    dynmaique au clique à régler

---
+++ main.js
@@ -0,0 +1,14 @@
+const circleCenter = document.getElementById("circleCenter")
+const circleOne = document.getElementById("circleOne");
+const circleTwo = document.getElementById("circleTwo");
+const circleThree = document.getElementById("circleThree");
+console.log(circleCenter);
+console.log(circleOne);
+console.log(circleTwo);
+console.log(circleThree);
+
+circleCenter.addEventListener("click", () => {
+    circleOne.classList.add("hidden");
+    circleTwo.classList.add("hidden");
+     circleThree.classList.add("hidden");
+});
